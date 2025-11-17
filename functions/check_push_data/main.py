511f3092388476462dcba5ebbed537ffa436a97c
Cristobal Tello <cristobal.tello@outlook.com>
2025-11-17 08:55:39 +0100

    Upload changhes

--- functions/check_push_data/main.py
+++ functions/check_push_data/main.py
@@ -1,16 +1,19 @@
 import logging
 import inspect # To dynamically get the function name
 import json
+import os
+import random
 from google.events.cloud.firestore import DocumentEventData, Document
 from google.events.cloud.firestore_v1.types.data import Value
 import functions_framework
+import pusher
 from typing import Dict, Any, Optional
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
+_pusher_client = None
 
 def _decode_firestore_fields(fields: Dict[str, Any]) -> Dict[str, Any]:
     """
@@ -30,6 +33,31 @@ def _decode_firestore_fields(fields: Dict[str, Any]) -> Dict[str, Any]:
         else:
             decoded_data[key] = firestore_type_value
     return decoded_data
+
+def get_pusher_client():
+    """Initializes and returns the Pusher client from environment variables."""
+    global _pusher_client
+    if _pusher_client is None:
+        try:
+            _pusher_client = pusher.Pusher(
+                app_id=os.environ["PUSHER_APP_ID"],
+                key=os.environ["PUSHER_APP_KEY"],
+                secret=os.environ["PUSHER_APP_SECRET"],
+                cluster=os.environ["PUSHER_CLUSTER"],
+                ssl=True,
+            )
+            logger.info("Pusher client configuration: App ID=%s, Key=%s, Cluster=%s",
+                        os.environ["PUSHER_APP_ID"],
+                        os.environ["PUSHER_APP_KEY"],
+                        os.environ["PUSHER_CLUSTER"])
+            logger.info("Pusher client initialized successfully.")
+        except KeyError as e:
+            logger.error(f"Missing required Pusher environment variable: {e}")
+            raise
+        except Exception as e:
+            logger.error(f"Failed to initialize Pusher client: {e}")
+            raise
+    return _pusher_client
 
 def unwrap_value(value_obj: Value) -> Any:
     kind = value_obj._pb.WhichOneof("value_type")
@@ -90,6 +118,16 @@ def check_push_data(cloudevent):
         logger.info("Resource name: %s", resource_name)
         for key, content in decoded_fields.items():
             logger.info(f"Field: {key}, Content: {content}")
+
+        # Generate a random number between 1 and 100 for rate-limiting simulation
+        rate_limit_value= random.randint(1, 100)
+
+        # --- Send Notification via Pusher ---
+        pusher_client = get_pusher_client()
+
+        payload = {'total': rate_limit_value}
+        pusher_client.trigger(os.environ.get("PUSHER_CHANNEL"), os.environ.get("PUSHER_EVENT"), payload)
+        logger.info(f"Successfully sent notification to Pusher on channel payload: {payload}")
         
         logger.info(f"******** End Processing: {func_name} *************")
         return "OK", 200
